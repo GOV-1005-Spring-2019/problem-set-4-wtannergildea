56b6cf6a219938959b86863c330a3bc77c20328d
Tanner Gildea <tanner@Tanners-MacBook-Pro.local>
2019-02-26 17:01:51 -0500

    Draft question 2 complete

--- pset_4.R
+++ pset_4.R
@@ -7,70 +7,55 @@ library(lubridate)
 
 polling_data <- read_csv("ps_4_elections-poll-nc09-3.csv")
 
-# View(polling_data)
-
-
 # 1. There were X respondents who supported the Democratic candidate.
-# dem_candidate_supporter <- polling_data %>%  
-#  filter(response == "Dem") %>% 
-# nrow()
-#answer = 219
+dem_candidate_supporter <- polling_data %>%
+ filter(response == "Dem") %>%
+nrow()
 
 # 2. There were X more respondents who favored the Republican candidate than who were Undecided.
-#favor_rep <- polling_data %>% 
-# filter(hrep_fav == "Favorable") %>% 
-# nrow() 
+favor_rep <- polling_data %>%
+filter(hrep_fav == "Favorable") %>%
+nrow()
 
-#favor_undecided <- polling_data %>% 
-#  filter(hrep_fav == "Don't know") %>% 
-#  nrow()
-  
-#rep_over_undecided <- (favor_rep - favor_undecided) %>% 
-# View()
+favor_undecided <- polling_data %>%
+ filter(hrep_fav == "Don't know") %>%
+ nrow()
 
-# answer = 104
+rep_over_undecided <- (favor_rep - favor_undecided)
 
-# 3. There are two gender variables (gender and gender_combined). 
+# 3. There are two gender variables (gender and gender_combined).
 # There are X individuals for whom these variables have different values.
 
-#different_gender <- polling_data %>% 
-#  select(gender, gender_combined) %>% 
-#  filter(gender != gender_combined) %>% 
-#  nrow() %>% 
-#  View()
-
-# answer = 10
+different_gender <- polling_data %>%
+ select(gender, gender_combined) %>%
+ filter(gender != gender_combined) %>%
+ nrow() 
 
 # 4. There are X respondents listed as “White” under race_eth who are not listed as “White” under file_race_black.
-# polling_data %>% 
-#  select(race_eth, file_race_black) %>% 
-#  filter(race_eth == "White", file_race_black != "White") %>% 
-#  nrow() %>% 
-#  View()
-
-#answer = 28
+polling_data %>%
+ select(race_eth, file_race_black) %>%
+ filter(race_eth == "White", file_race_black != "White") %>%
+ nrow() 
 
 # 5.  The first response of Dem came X minutes (rounded to the nearest minute)
 # before the first response of Rep.
 # first Dem was at 2018-10-26 22:06:37
 # first Rep was at 2018-10-26 22:17:39
 
-# dem_time <- polling_data %>% 
-#   filter(response == "Dem") %>% 
-#   arrange(timestamp) %>% 
-#   slice(1) %>% 
-#   pull(timestamp)
-# 
-# 
-# rep_time <- polling_data %>% 
-#   filter(response == "Rep") %>% 
-#   arrange(timestamp) %>% 
-#   slice(1) %>% 
-#   pull(timestamp)
-# 
-# time_difference <-  (minute(rep_time) - minute(dem_time))
-# 
-# answer = 11
+dem_time <- polling_data %>%
+  filter(response == "Dem") %>%
+  arrange(timestamp) %>%
+  slice(1) %>%
+  pull(timestamp)
+
+rep_time <- polling_data %>%
+  filter(response == "Rep") %>%
+  arrange(timestamp) %>%
+  slice(1) %>%
+  pull(timestamp)
+
+time_difference <- (minute(rep_time) - minute(dem_time))
+
 
 # Question 2 box 
 
@@ -88,6 +73,7 @@ table <- polling_data %>%
   mutate(Rep = Rep / all) %>%
   mutate(Und = Und / all) %>%
   select(-all, - `3`) %>%
+  arrange(factor(race_eth, levels = c("White", "Black", "Hispanic", "Asian", "Other"))) %>% 
 
   # One of the biggest pieces of black magic incantation in R is ungroup(). (I
   # did not mention this in class.) Summary: Whenever you group a tibble (as we
@@ -102,16 +88,23 @@ table <- polling_data %>%
 
 gt() %>%
   tab_header(
-    title = "Polling Results in NC 9th") %>%
+    title = "Polling Results in North Carolina 9th Congressional District") %>%
 
   cols_label(
-    race_eth = NULL,
+    race_eth = "Race",
     Dem = "DEM.",
     Rep = "REP.",
     Und = "UND."
   ) %>%
 
   fmt_percent(columns = vars(Dem, Rep, Und),
-              decimals = 0)
+              decimals = 0)  
 
 View(table)
+
+
+
+
+
+
+
