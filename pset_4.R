084ca260e5ee327e8c99046d99f46c61420f4e86
Tanner Gildea <tanner@Tanners-MacBook-Pro.local>
2019-02-26 22:28:13 -0500

    Final submission.

--- pset_4.R
+++ pset_4.R
@@ -4,8 +4,11 @@ library(gt)
 library(tidyverse)
 library(readxl)
 library(lubridate)
+library(ggplot2)
 
 polling_data <- read_csv("ps_4_elections-poll-nc09-3.csv")
+
+# Madlibs
 
 # 1. There were X respondents who supported the Democratic candidate.
 dem_candidate_supporter <- polling_data %>%
@@ -14,11 +17,11 @@ nrow()
 
 # 2. There were X more respondents who favored the Republican candidate than who were Undecided.
 favor_rep <- polling_data %>%
-filter(hrep_fav == "Favorable") %>%
+filter(response == "Rep") %>%
 nrow()
 
 favor_undecided <- polling_data %>%
- filter(hrep_fav == "Don't know") %>%
+ filter(response == "Und") %>%
  nrow()
 
 rep_over_undecided <- (favor_rep - favor_undecided)
@@ -29,13 +32,13 @@ rep_over_undecided <- (favor_rep - favor_undecided)
 different_gender <- polling_data %>%
  select(gender, gender_combined) %>%
  filter(gender != gender_combined) %>%
- nrow() 
+ nrow()
 
 # 4. There are X respondents listed as “White” under race_eth who are not listed as “White” under file_race_black.
 polling_data %>%
  select(race_eth, file_race_black) %>%
  filter(race_eth == "White", file_race_black != "White") %>%
- nrow() 
+ nrow()
 
 # 5.  The first response of Dem came X minutes (rounded to the nearest minute)
 # before the first response of Rep.
@@ -57,7 +60,7 @@ rep_time <- polling_data %>%
 time_difference <- (minute(rep_time) - minute(dem_time))
 
 
-# Question 2 box 
+# Question 2 box
 
 table <- polling_data %>%
   select(response, race_eth, final_weight) %>%
@@ -73,18 +76,12 @@ table <- polling_data %>%
   mutate(Rep = Rep / all) %>%
   mutate(Und = Und / all) %>%
   select(-all, - `3`) %>%
-  arrange(factor(race_eth, levels = c("White", "Black", "Hispanic", "Asian", "Other"))) %>% 
+  arrange(factor(race_eth, levels = c("White", "Black", "Hispanic", "Asian", "Other"))) %>%
 
-  # One of the biggest pieces of black magic incantation in R is ungroup(). (I
-  # did not mention this in class.) Summary: Whenever you group a tibble (as we
-  # do above) the grouping stays with an resulting object, until you explicitly
-  # ungroup() it. That can't ever hurt things (right? TFs?) and it often helps,
-  # as in this case.
 
   ungroup() %>%
 
-  # You will have a chance to explore many other gt commands in problem set #4.
-  # I added two extras that we did not get to in class.
+
 
 gt() %>%
   tab_header(
@@ -94,17 +91,69 @@ gt() %>%
     race_eth = "Race",
     Dem = "DEM.",
     Rep = "REP.",
-    Und = "UND."
-  ) %>%
+    Und = "UND."   ) %>%
 
   fmt_percent(columns = vars(Dem, Rep, Und),
-              decimals = 0)  
-
-View(table)
+              decimals = 0)
 
 
 
 
+# Question 3 voter education level
+voter_education_violin <- polling_data %>%
+  select(educ, final_weight) %>%
+  filter(educ != "[DO NOT READ] Refused") %>%
+  group_by(educ)
 
+  # trying to change order
+voter_education_violin$educ <-
+  fct_rev(factor(voter_education_violin$educ, levels = c("Graduate or Professional Degree",
+                                  "Bachelors' degree",
+                                  "Some college or trade school",
+                                  "High school",
+                                  "Grade school")))
+
+voter_education_violin %>%
+  ggplot(aes(x = educ, y = final_weight)) +
+
+  geom_violin() +
+
+  coord_flip() +
+
+  geom_jitter(alpha = .5, width = .2, size = 1) +
+
+  labs(title = "More Educated Matter Less in North Carolina 9th",
+       subtitle = "Poll gives more weight to people who are less likely to participate in polls",
+       caption = "New York Times Upshot/Siena College 2018 live polls") +
+
+  ylab("Weight Given to Respondent in Calculating Poll Results") + xlab(NULL)
+
+
+
+# Question 4
+
+# Republican response based on educ and age
+categorical_data <- polling_data %>% 
+  select(ager, educ, response)  %>% 
+  filter(response == "Rep") %>% 
+  filter(educ != "[DO NOT READ] Refused") %>% 
+  filter(ager != "[DO NOT READ] Refused") %>% 
+  group_by(educ, ager) %>% 
+  mutate(count = n())
+
+categorical_data$educ <- factor(categorical_data$educ, levels = c("Graduate or Professional Degree",                                    "Bachelors' degree", 
+                                  "Some college or trade school", 
+                                   "High school", 
+                                   "Grade school"))
+  
+ ggplot(categorical_data, aes(x = ager, y = count, fill = educ)) + 
+   geom_col(position = "dodge", width = .5) +
+ 
+   labs(title = "Republican Voters by Education Level and Age",  
+        subtitle = "Per Response to Poll",
+        caption = "New York Times Upshot/Siena College 2018 live polls",
+        fill = "Education Level") +
+
+   xlab("Age") + ylab("Count") 
 
 
